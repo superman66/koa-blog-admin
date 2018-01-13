b0c14019b2a8574acac0dd07e46e0a8c21193645
Superman <supermanchc@gmail.com>
2018-01-13 21:26:11 +0800

    Update User module

--- src/actions/users.js
+++ src/actions/users.js
@@ -11,15 +11,41 @@ export function fetchUsers(params) {
   }
 }
 
+export function fetchUserById(id) {
+  return {
+    actionType: types.FETCH_USER_BY_ID,
+    options: {
+      url: APIs.API_FETCH_USER_ID,
+      params: {
+        id
+      }
+    }
+  }
+}
+
 export function deleteUser(id) {
   return {
-    actionType: types.DELETE_USERS,
+    actionType: types.DELETE_USER,
     options: {
       method: 'delete',
       url: APIs.API_DELETE_USER,
       params: {
         id
       }
+    }
+  }
+}
+
+export function updateUser(id, body) {
+  return {
+    actionType: types.UPDATE_USER,
+    options: {
+      method: 'patch',
+      url: APIs.API_UPDATE_USER,
+      params: {
+        id
+      },
+      body
     }
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,8 +1,9 @@
-
 import types from '../constants/ActionTypes'
 import createReducer from '../utils/createReducer'
 import createRequestHandler from '../utils/createRequestHandler'
-import { toTable } from '../utils/actionToStore'
+import {
+  toTable
+} from '../utils/actionToStore'
 
 const initialState = {
   status: null,
@@ -15,7 +16,14 @@ export default createReducer(initialState, {
   [types.FETCH_USERS]: createRequestHandler((state, action) => {
     return toTable(action)
   }),
-  [types.DELETE_USERS]: createRequestHandler((state, action) => {
+  [types.DELETE_USER]: createRequestHandler((state, action) => {
     return action
+  }),
+  [types.FETCH_USER_BY_ID]: createRequestHandler((state, action) => {
+    return {
+      user: action.response.user || {},
+      status: action.status,
+      errors: action.errors || {}
+    }
   })
 })
