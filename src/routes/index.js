e3ba9dd523ebbef19970e8ccbccffe5eb3e1b222
Superman <supermanchc@gmail.com>
2018-01-29 22:13:20 +0800

    refactor(IndexRoute): change IndexRoute to Dashboard

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,28 +5,25 @@ import tag from './tag'
 import comment from './comment'
 import post from './post'
 import about from './about'
+import { redirectToLogin } from '../utils/auth'
 
 export default {
-  childRoutes: [{
-    path: '/',
-    // onEnter: redirectToLogin,
-    component: require('../containers/Index').default,
-    indexRoute: {
-      onEnter: (nextState, replace) => replace('/dashboard')
+  childRoutes: [
+    {
+      path: '/',
+      // onEnter: redirectToLogin,
+      component: require('../containers/Index').default,
+      indexRoute: {
+        component: require('../containers/Dashboard').DashboardPage,
+        onEnter: redirectToLogin,
+      },
+      childRoutes: [users, dashboard, category, tag, comment, post, about],
     },
-    childRoutes: [
-      users,
-      dashboard,
-      category,
-      tag,
-      comment,
-      post,
-      about,
-    ]
-  }, {
-    path: 'login',
-    getComponent(nextState, cb) {
-      cb(null, require('../containers/Login').default);
-    }
-  }]
-};
+    {
+      path: 'login',
+      getComponent(nextState, cb) {
+        cb(null, require('../containers/Login').default)
+      },
+    },
+  ],
+}
